dab429b4c41918de7d4672980c155041dbd1a7b1
Santosh Yadav <santosh.yadav198613@gmail.com>
2017-10-07 23:15:31 +0530

    added component interaction and lifcycle hooks

---
+++ src/app/product/product-list/product-list.component.ts
@@ -0,0 +1,29 @@
+import {
+  Component, OnInit, Input, Output,
+  EventEmitter, OnChanges, SimpleChanges
+} from '@angular/core';
+import { Product } from '../product';
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent implements OnInit, OnChanges {
+  @Input() productList: Product[];
+  @Input() title: string;
+  @Output() visibe = new EventEmitter<boolean>();
+  @Output() message = new EventEmitter<string>();
+
+  ngOnChanges(changes: SimpleChanges): void {
+   console.log(changes);
+  }
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  sendDataToParent() {
+    this.visibe.emit(true);
+    this.message.emit('This is message from child');
+  }
+}
--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,14 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, DoCheck, OnDestroy, OnInit, QueryList, ViewChild, ViewChildren, AfterViewInit } from '@angular/core';
 import { Product } from './product';
+
+import { ProductListComponent } from './product-list/product-list.component';
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.css']
 })
-export class ProductComponent {
+export class ProductComponent implements OnInit, DoCheck, OnDestroy, AfterViewInit {
   productName: string = 'LED TV 56inches';
   hidden: boolean = true;
+  message: string;
+  title: string = 'Product List';
   products: Product[] = [{
     id: 1, name: 'LED TV', price: 10000
   },
@@ -16,10 +20,52 @@ export class ProductComponent {
     id: 2, name: 'AC', price: 20000
   }];
 
+  @ViewChild(ProductListComponent)
+  productListComponent: ProductListComponent;
+
+  @ViewChildren(ProductListComponent)
+  productListChildren: QueryList<ProductListComponent>;
+
   constructor() { }
+
+  ngOnInit(): void {
+    console.log(this.productListComponent);
+    console.log('This is viewChildren instance' + this.productListChildren);
+    this.productListComponent.productList = this.products;
+    this.productListComponent.title = this.title;
+  }
+
+  ngAfterViewInit(): void {
+    console.log(this.productListChildren);
+
+
+    this.productListChildren.forEach((data) => setTimeout(() => {
+      data.title = 'Product List Children';
+      data.productList = this.products;
+    }, 0));
+
+  }
+
+  ngDoCheck(): void {
+    console.log('this is do check event');
+  }
 
   toggle() {
     this.hidden = !this.hidden;
+    this.title = 'Product List Modified';
   }
+
+  receiveFromChild(visible: boolean) {
+    console.log(visible);
+  }
+
+  messageFromChild(message: string) {
+    this.message = message;
+  }
+
+  ngOnDestroy(): void {
+    alert('Are you sure you want to close this component');
+  }
+
 
 }
